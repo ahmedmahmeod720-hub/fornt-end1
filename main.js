d69ed201ea09ee50eaf9341e39b4c2f5dde881db
ahmedmahmeod720-hub <ahmedmahmeod720@gmail.com>
2026-09-12 19:18:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const WHATSAPP_NUMBER = '201143348433';
 
 // عنوان الباك إند (سبرينج بوت)
 // غيّر البورت لو شغّال على بورت تاني
-const API_BASE = 'http://localhost:8080';
+const API_BASE = 'back-end2-production.up.railway.app';
 
 // ===== البيانات المحملة من الباك إند (كاش في الذاكرة) =====
 let bricks = [];
